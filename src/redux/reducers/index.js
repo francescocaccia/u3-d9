623af01331538929af3aa9ae49c206df9dc3889b
francescocaccia <123968277+francescocaccia@users.noreply.github.com>
2023-04-06 14:19:07 +0200

    test

---
+++ src/redux/reducers/index.js
@@ -0,0 +1,9 @@
+const initialState = {};
+
+const mainReducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+export default mainReducer;
